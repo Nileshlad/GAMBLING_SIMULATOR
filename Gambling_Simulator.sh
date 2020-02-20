1b236bde33abcd543f8ed2014709afa801c3fa3f
Nileshlad <ladnilesh1994.com>
2020-02-20 09:43:32 +0530

    ADDED:To As a Gambler check the win and loose 50% of the stake

--- Gambling_Simulator.sh
+++ Gambling_Simulator.sh
@@ -4,7 +4,8 @@ echo"-----------------------------------WELL COME GAMBLING SIMULATION-----------
 #CONSTANT
 EVERY_DAY_STAKE=100;
 EVERY_DAY_BET=1
-
+WIN_CASH=$(($EVERY_DAY_STAKE + $EVERY_DAY_STAKE/2))
+LOST_CASH=$(($EVERY_DAY_STAKE - $EVERY_DAY_STAKE/2))
 #VARIABLE
 isWin=1
 
@@ -13,14 +14,16 @@ cash=$EVERY_DAY_STAKE
 
 #TO RANDOM VALUE 0 AND 1
 checkWinLoss=$((RANDOM%2))
-if [ $checkWinLoss -eq $isWin ]
-then
-		echo "win"
-		((cash++))
-else
-		echo "loss"
-		((cash--))
-fi
+while [[ $cash -ne $WIN_CASH && $cash -ne $LOST_CASH ]]
+do
+	if [ $checkWinLoss -eq $isWin ]
+		then
+			((cash++))
+	else
+			((cash--))
+	fi
+done
 
-#echo $EVERY_DAY_BET
+#TO PRINT
 echo $cash
+
