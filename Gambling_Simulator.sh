2e726f100ccb16bd2709e4ba6fba8b9328d63413
Nileshlad <ladnilesh1994.com>
2020-02-20 11:16:03 +0530

    ADDED:To win and loss day by month

--- Gambling_Simulator.sh
+++ Gambling_Simulator.sh
@@ -1,25 +1,45 @@
 #!/bin/bash -x
 echo"-----------------------------------WELL COME GAMBLING SIMULATION--------------------------------"
 
+declare -a TotalCashMonth
 #CONSTANT
-EVERY_DAY_STAKE=100;
+EVERY_DAY_STAKE=100
 EVERY_DAY_BET=1
+IS_WIN=1
+DAYS_IN_MONTH=30
 
 #VARIABLE
-isWin=1
+maxWin=$(($EVERY_DAY_STAKE+$EVERY_DAY_STAKE/2))
+maxLoose=$(($EVERY_DAY_STAKE-$EVERY_DAY_STAKE/2))
 
+#CHECK IF WIN OR LOSE UNTIL CONDITION
+for (( index=1; index<=$DAYS_IN_MONTH; index++))
+do
 cash=$EVERY_DAY_STAKE
 
-#TO RANDOM VALUE 0 AND 1
-checkWinLoss=$((RANDOM%2))
-if [ $checkWinLoss -eq $isWin ]
+while [[ $cash -ne $maxWin  &&  $cash -ne $maxLoose ]]
+do
+	randomNumber=$((RANDOM%2))
+if [ $randomNumber -eq $IS_WIN ]
 then
-		echo "win"
-		((cash++))
+	((winCount++))
+	((cash++))
 else
-		echo "loss"
-		((cash--))
+	((loseCount++))
+	((cash--))
 fi
+done
+	winCount=0
+	loseCount=0
+	cash=$(( cash - EVERY_DAY_STAKE ))
+	totalcash=$(($totalcash+$(($cash))))
+	TotalCashMonth[index]={$totalcash}
+if [ $cash -gt 0 ]
+then
+		echo "win :$cash"
+else
+		echo "lose :$cash"
+fi
+done
+echo "Total Cash:$totalcash"
 
-#echo $EVERY_DAY_BET
-echo $cash
