99b8e1f5db412ffa1331e2f4768f4227875af07e
Nileshlad <ladnilesh1994@gmail.com>
2020-02-21 12:00:37 +0530

    ADDED:To gambling minimum lost and maximum won

--- Gambling_Simulator.sh
+++ Gambling_Simulator.sh
@@ -1,45 +1,89 @@
-#!/bin/bash -x
-echo"-----------------------------------WELL COME GAMBLING SIMULATION--------------------------------"
+#!/bin/bash
 
-declare -a TotalCashMonth
-#CONSTANT
+echo "--------------------------------------------------------WELL COME GAMBLING SIMULATION-------------------------------------------------"
+
+#DECLARE A DICTIONARY
+declare -A dailyCash
+
 EVERY_DAY_STAKE=100
 EVERY_DAY_BET=1
 IS_WIN=1
 DAYS_IN_MONTH=30
+MAXWIN=$(($EVERY_DAY_STAKE+$EVERY_DAY_STAKE/2))
+MAXLOOSE=$(($EVERY_DAY_STAKE-$EVERY_DAY_STAKE/2))
+MONTH_IN_YEAR=1
 
 #VARIABLE
-maxWin=$(($EVERY_DAY_STAKE+$EVERY_DAY_STAKE/2))
-maxLoose=$(($EVERY_DAY_STAKE-$EVERY_DAY_STAKE/2))
+cash=0
+limit=0
+dayWin=0
+dayLoose=0
+flag=0
+
+#FUNCTION TO GET DAILY CASH WIN OR LOSE
+function gamble()
+{
+	#GENERATE RANDOM NUMBER FOR GAMBLER
+	randomNumber=$((RANDOM%2))
+	cash=$EVERY_DAY_STAKE
+	while [[ $cash -ne $MAXWIN && $cash -ne $MAXLOOSE ]]
+	do
+		if [ $randomNumber -eq $IS_WIN ]
+		then
+			cash=$(($cash+$EVERY_DAY_BET))
+		else
+			cash=$(($cash-$EVERY_DAY_BET))
+		fi
+	done
+	echo "$cash"
+}
 
 #CHECK IF WIN OR LOSE UNTIL CONDITION
-for (( index=1; index<=$DAYS_IN_MONTH; index++))
-do
-cash=$EVERY_DAY_STAKE
 
-while [[ $cash -ne $maxWin  &&  $cash -ne $maxLoose ]]
+	for ((month=1; month<=$MONTH_IN_YEAR; month++))
+	do
+		for (( day=1; day<=$DAYS_IN_MONTH; day++))
+		do
+   		cashPerDay=$(gamble)
+			if [ $cashPerDay -gt $EVERY_DAY_STAKE  ]
+			then
+				((dayWin++))
+			else
+				((dayLoose++))
+			fi
+   		cashPerDay=$(( $cashPerDay - $EVERY_DAY_STAKE ))
+   		dailyCash[Day $day]=$cashPerDay 
+   		totalCash=$(($totalCash+$(($cashPerDay))))
+			totalCashMonth[$day]=$totalCash
+		done
+		echo "........................Month $month........................."
+		echo " "
+		echo "Total days won $dayWin days by $(($dayWin*50))"
+		echo "Total days Loose $dayLoose days  by $(($dayLoose*50))"
+		echo "Total Cash for a month:$totalCash"
+		dayWin=0
+		dayLoose=0
+		echo " "
+		min=${totalCashMonth[0]}
+		max=${totalCashMonth[0]}
+		maxday=0
+		minday=0
+for ((j=0; j<${#totalCashMonth[@]}; j++))
 do
-	randomNumber=$((RANDOM%2))
-if [ $randomNumber -eq $IS_WIN ]
-then
-	((winCount++))
-	((cash++))
-else
-	((loseCount++))
-	((cash--))
-fi
+	if [ $(echo "${totalCashMonth[j]} -gt $max") ]
+	then
+		max=$((${totalCashMonth[j]}))
+		maxday=$j
+	fi
+	if [ $(echo "${totalCashMonth[j]} -lt $min") ]
+	then
+		min=$((${totalCashMonth[j]}))
+		minday=$j
+	fi	
 done
-	winCount=0
-	loseCount=0
-	cash=$(( cash - EVERY_DAY_STAKE ))
-	totalcash=$(($totalcash+$(($cash))))
-	TotalCashMonth[index]={$totalcash}
-if [ $cash -gt 0 ]
-then
-		echo "win:$cash"
-else
-		echo "lost:$cash"
-fi
+echo "${totalCashMonth[@]}"
+echo "most lucky is day is  $maxday cash:$max"
+echo "most unlucky is day is  $minday cash:$min"
+echo " "
 done
-echo "Total Cash:$totalcash"
 
